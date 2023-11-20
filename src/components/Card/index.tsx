44ca445f6c307534105c6b3dfb1d3144516daa64
erikfelipe <erikfelipebh@gmail.com>
2023-11-20 13:00:13 -0300

    Add Component Card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,42 +1,11 @@
 import React from "react";
 import "./style.css";
 
-const Card = () => {
+const Card = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex justify-center items-center h-screen bg-color">
       <div className="p-4 bg-white flex flex-col items-center rounded-lg shadow-md">
-        <section>
-          <div>
-            <h1>Stay updated!</h1>
-          </div>
-          <div>
-            <h2>Join 60,000+ product managers receiving monthly updates on:</h2>
-          </div>
-          <div>
-            <ul>
-              <div>
-                <img src="" alt="" />
-                <li>Product discovery and building what matters</li>
-              </div>
-              <div>
-                <img src="" alt="" />
-                <li>Measuring to ensure updates are a success</li>
-              </div>
-              <div>
-                <img src="" alt="" />
-                <li>And much more!</li>
-              </div>
-            </ul>
-          </div>
-          <div>
-            <p>Email address</p>
-            <input placeholder="erikfelipebh@gmail.com" />
-          </div>
-          <button title="Subscribe to monthly newsletter" />
-        </section>
-        <section>
-          <img src="" alt="" />
-        </section>
+        {children}
       </div>
     </div>
   );
--- src/pages/NewsletterForm/index.tsx
+++ src/pages/NewsletterForm/index.tsx
@@ -1,8 +1,47 @@
 import React from "react";
 import Card from "../../components/Card/index.tsx";
+import illustrationDsk from "../../assets/images/illustration-sign-up-desktop.svg";
+import iconList from "../../assets/images/icon-list.svg";
 
 const NewsletterForm = () => {
-  return <Card />;
+  return (
+    <Card>
+      <section className="flex ">
+        <section className="flex flex-col items-center">
+          <div>
+            <h1 className="font_Roboto-Bold text-4xl">Stay updated!</h1>
+          </div>
+          <div>
+            <h2>Join 60,000+ product managers receiving monthly updates on:</h2>
+          </div>
+          <div>
+            <ul>
+              <div className="flex">
+                <img src={iconList} alt="iconList" className="mr-2" />
+                <li>Product discovery and building what matters</li>
+              </div>
+              <div>
+                <img src={iconList} alt="iconList" />
+                <li>Measuring to ensure updates are a success</li>
+              </div>
+              <div>
+                <img src={iconList} alt="iconList" />
+                <li>And much more!</li>
+              </div>
+            </ul>
+          </div>
+          <div>
+            <p>Email address</p>
+            <input placeholder="erikfelipebh@gmail.com" />
+          </div>
+          <button title="Subscribe to monthly newsletter" />
+        </section>
+        <section className="ml-3">
+          <img src={illustrationDsk} alt="imageDsk" className="w-60 " />
+        </section>
+      </section>
+    </Card>
+  );
 };
 
 export default NewsletterForm;
