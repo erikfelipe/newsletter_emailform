8f6aadfa01a5b099a9be066ced5d219aa3259442
erikfelipe <erikfelipebh@gmail.com>
2023-11-20 16:44:07 -0300

    Refactoring Component Button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import * as S from "./style.ts";
+
+const Button = () => {
+  return <S.Button type="submit">Subscribe to monthly newsletter</S.Button>;
+};
+
+export default Button;
---
+++ src/components/Button/style.ts
@@ -0,0 +1,10 @@
+import { styled } from "styled-components";
+
+export const Button = styled.button`
+  margin-top: 30px;
+  padding: 10px;
+  border: none;
+  background: #ff6257;
+  color: white;
+  font-family: "Roboto-Regular";
+`;
